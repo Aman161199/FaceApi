4ac963aaf629f13bd795abe813557da8ef4c4d05
Aman161199 <43999188+Aman161199@users.noreply.github.com>
2021-04-19 19:25:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,16 +118,7 @@ def video_feed():
 
 # check to see if this is the main thread of execution
 if __name__ == '__main__':
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-f", "--frame-count", type=int, default=32,
-		help="# of frames used to construct the background model")
-	args = vars(ap.parse_args())
-
-	# start a thread that will perform motion detection
-	t = threading.Thread(target=detect_motion, args=(
-		args["frame_count"],))
-	t.daemon = True
-	t.start()
+	
 
 	# start the flask app
 	app.run()
